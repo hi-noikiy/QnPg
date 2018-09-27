70e751c0fdd8720da93cc2dacf3d01f6692f365a
renchengxiaofeixia <renchengxiaofeixia@gmail.com>
2018-09-27 20:24:56 +0800

    小飞虾

---
+++ source/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace 获取千牛聊天记录
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbQnAccount = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGetChatlog = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbQnAccount
+            // 
+            this.cmbQnAccount.FormattingEnabled = true;
+            this.cmbQnAccount.Location = new System.Drawing.Point(119, 12);
+            this.cmbQnAccount.Name = "cmbQnAccount";
+            this.cmbQnAccount.Size = new System.Drawing.Size(197, 20);
+            this.cmbQnAccount.TabIndex = 0;
+            this.cmbQnAccount.SelectedIndexChanged += new System.EventHandler(this.cmbQnAccount_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "当前登录千牛账号";
+            // 
+            // btnGetChatlog
+            // 
+            this.btnGetChatlog.Location = new System.Drawing.Point(453, 9);
+            this.btnGetChatlog.Name = "btnGetChatlog";
+            this.btnGetChatlog.Size = new System.Drawing.Size(104, 23);
+            this.btnGetChatlog.TabIndex = 2;
+            this.btnGetChatlog.Text = "获取聊天记录";
+            this.btnGetChatlog.UseVisualStyleBackColor = true;
+            this.btnGetChatlog.Click += new System.EventHandler(this.btnGetChatlog_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 52);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(562, 437);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "获取登录的千牛";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 501);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnGetChatlog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbQnAccount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbQnAccount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGetChatlog;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
